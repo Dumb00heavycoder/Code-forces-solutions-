2adefab158f5ba11c98e8427b71cb1582bb6afc5
dhruv <dhruvlgd44@gmail.com>
2025-07-29 16:59:01 +0530

    sol

--- Prb_263A.cpp
+++ Prb_263A.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 int main(){
-    int x;
-    int row, column;
+    int x,row, column;
     for (int i = 0; i<5; ++i){
         for(int j=0; i<5; ++j){
-            int x;
-            if(x=0){
+            cin >> x;
+            if(x==1){
                 row = i;
                 column = j;
             }
         }
     }
-    int moves= abs(row-2) + abs(column-2);
-    cout << moves << endl;
+    int moves = abs(row-2) + abs(column-2);
+    cout << moves<< endl;
     return 0;
 }
