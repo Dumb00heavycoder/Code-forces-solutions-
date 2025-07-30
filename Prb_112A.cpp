9e26e72e0a44c35e3e92cc9956687fb6a39c372b
dhruv <dhruvlgd44@gmail.com>
2025-07-30 22:27:38 +0530

    i make alot of mistakes

--- Prb_112A.cpp
+++ Prb_112A.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main (){
     string str1, str2;
     cin >> str1 >> str2;
-    for(int i = 0, i < str1.length(); ++i){
+    for(int i = 0; i < str1.length(); ++i){
         str1[i] = toupper(str1[i]);
     }
-    for(int i = 0, i < str2.length(); ++i){
+    for(int i = 0; i < str2.length(); ++i){
         str2[i] = toupper(str2[i]);
     }
 
