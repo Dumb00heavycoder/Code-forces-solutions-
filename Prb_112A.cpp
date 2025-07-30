69af3aff7a4a8495b803675adfb1b621a10b35a7
dhruv <dhruvlgd44@gmail.com>
2025-07-30 22:21:09 +0530

    prb

--- Prb_112A.cpp
+++ Prb_112A.cpp
@@ -6,10 +6,17 @@ using namespace std;
 int main (){
     string str1, str2;
     cin >> str1 >> str2;
-    if (toupper(str1) < toupper(str2) ){
+    for(int i = 0, i >sizeof(str1); ++i){
+        str1[i] = isupper(str1[i]);
+    }
+    for(int i = 0, i > sizeof(str2); ++i){
+        str2[i] = isupper(str2[i]);
+    }
+
+    if (str1 < str2 ){
         cout << -1;
     }
-    else if (toupper(str2) < toupper(str1)){
+    else if (str2 < str1){
         cout << 1;
     }
     else{
