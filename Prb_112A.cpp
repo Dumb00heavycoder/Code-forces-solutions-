a106124ae5a845ff616a21500caaa98ef2e68965
dhruv <dhruvlgd44@gmail.com>
2025-07-30 21:06:47 +0530

    bruh why was i using int

--- Prb_112A.cpp
+++ Prb_112A.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main (){
-    int str1, str2;
+    string str1, str2;
     cin >> str1 >> str2;
     if (toupper(str1) < toupper(str2) ){
         cout << -1;
     }
-    else if (str2 < str1){
+    else if (toupper(str2) < toupper(str1)){
         cout << 1;
     }
     else{
