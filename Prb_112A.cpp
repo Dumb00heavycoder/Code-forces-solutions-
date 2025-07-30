f1b6823746c9aa2dfa2d1628aa655f32618f3a24
dhruv <dhruvlgd44@gmail.com>
2025-07-30 21:03:43 +0530

    error

--- Prb_112A.cpp
+++ Prb_112A.cpp
@@ -1,11 +1,12 @@
 #include <iostream> 
 #include <string>
+#include <cctype>
 using namespace std;
 
 int main (){
     int str1, str2;
     cin >> str1 >> str2;
-    if (str1 < str2 ){
+    if (toupper(str1) < toupper(str2) ){
         cout << -1;
     }
     else if (str2 < str1){
