ca02edfbb9074a40054a280f8ef5820a89ee6e37
dhruv <dhruvlgd44@gmail.com>
2025-07-30 21:00:02 +0530

    1st try

--- Prb_112A.cpp
+++ Prb_112A.cpp
@@ -1,5 +1,18 @@
 #include <iostream> 
-#include <strings>
+#include <string>
 using namespace std;
 
-
+int main (){
+    int str1, str2;
+    cin >> str1 >> str2;
+    if (str1 < str2 ){
+        cout << -1;
+    }
+    else if (str2 < str1){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+    return 0;
+}
