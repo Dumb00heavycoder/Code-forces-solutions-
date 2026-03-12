aaae5d6ddf714a0bf04d03c77482449124426726
Dhruv chaudhary <89791120+Dumb00heavycoder@users.noreply.github.com>
2026-03-12 13:02:30 +0530

    Add character counting program in Prb_266A.cpp
    Implement a program to count consecutive characters in a string.

---
+++ Prb_266A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+  int n;
+  int count = 0;
+  string s;
+  cin >> n;
+  cin >> s;
+  for(int i = 1; i<n; i++){
+    if(s[i] == s[i-1]){
+        count ++;
+    }
+  }
+  cout << count << endl;
+  return 0;
+}
