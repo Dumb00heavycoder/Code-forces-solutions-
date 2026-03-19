3b9cffbdefc96111ecf674c755e95fb81e84fcbe
dhruv <dhruvlgd44@gmail.com>
2026-03-19 19:03:52 +0530

    prbsolved

--- Prb_977A.cpp
+++ Prb_977A.cpp
@@ -6,16 +6,19 @@ int main () {
   int n, k;
   cin >> n;
   cin >> k;
-  while (k != 0){
-    if(n % 10 != 0){
-      n = n - 1;
-    }
-    else {
-      n = n /10;
-    }
-    k = k -1;
+  if (k >= 1 && k<= 50 ) {
+    while (k != 0){
+      if(n % 10 != 0){
+        n = n - 1;
+      }
+      else {
+        n = n /10;
+      }
+      k = k -1;
+    }}
+  else {
+    break;
   }
   cout << n << endl;
-  
   return 0;
 }
