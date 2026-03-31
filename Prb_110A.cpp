6156e2410492edf3ffa48e1200676838f3162a55
Dhruv chaudhary <dhruvlgd44@gmail.com>
2026-03-31 16:34:06 +0530

    another try for right solution

--- Prb_110A.cpp
+++ Prb_110A.cpp
@@ -3,24 +3,20 @@
 using namespace std;
 
 int main (){
-  string ans;
-  int n, count;
-  count = 0;
+  string n;
+  int count = 0;
   cin >> n;
-  while (n != 0){
-    if (n % 10 == 4 || n % 10 == 7){
-      count += 1;
-    }
-    else{
-	count = count;
-    }
-    n = int(n / 10);
+  for (char c : n){
+	  if(c == "4" || c == "7"){
+		  count++;
+	  }
   }
+
   if (count == 4 || count == 7){
-	  ans = "YES";
+	  cout <<"YES";
 	}
   else{
-	  ans = "NO";
+	  cout << "NO";
   }
   cout << ans;
   return 0;
